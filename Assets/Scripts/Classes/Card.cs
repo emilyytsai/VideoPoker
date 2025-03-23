ea56fae6cf7d0618ec6e81a281623326779efd7d
emilyytsai <emilytsai@csu.fullerton.edu>
2025-03-23 03:41:54 -0700

    Created a card class

---
+++ Assets/Scripts/Classes/Card.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Card
+{
+    //getters & setters
+    //get value of the property -> allow to set/change that value
+    public string Suit { get; private set; }
+    public string Rank { get; private set; }
+    public Sprite Card_sprite { get; private set; }
+
+    //constructor
+    //initialize the card w/ its suit rank and sprite
+    public Card(string suit, string rank, Sprite card_sprite)
+    {
+        Suit = suit;
+        Rank = rank;
+        Card_sprite = card_sprite;
+    }
+
+    //accessor method
+    //get the card names
+    public string GetCardName()
+    {
+        return Rank + " of " + Suit;
+        //would return smth like "Queen of Hearts"
+    }
+}
